c99e463d78573f27dce0d1237cd24224f21c5b18
juan pablo <juanpablo@MacBook-Pro-de-juan.local>
2021-08-22 16:19:59 -0500

    size image

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,24 +1,30 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 import React from "react";
-import { forwardRef } from 'react'
+import { forwardRef } from "react";
 export const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div ref={ref} className="p-4 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div
+      ref={ref}
+      className="p-4 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+    >
       <Image
+      className="object-cover"
         layout="responsive"
         src={`${BASE_URL}${result.poster_path || result.backdrop_path}`}
-        height="1080"
-        width="1920"
+        height="700"
+        width="500"
       />
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
-        <h2 className="mt-2 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.originl_name}</h2>
+        <h2 className="mt-2 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+          {result.title || result.originl_name}
+        </h2>
         <p className="flex items-center opacity-0 group-hover:opacity-10">
-          {result.media_type && `${result.media_type} `} {" "}
-          {result.release_date || result.first_air_date }{ " " }
-          <ThumbUpIcon className="h-5 mx-2" /> { result.vote_count }
+          {result.media_type && `${result.media_type} `}{" "}
+          {result.release_date || result.first_air_date}{" "}
+          <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
         </p>
       </div>
     </div>
